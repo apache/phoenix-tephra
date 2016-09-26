8e0e2cb7e815251194c8b15399abf5119f6a2ff6
Chris Gianelloni <chris@cask.co>
2016-09-26 14:30:47 -0400

    TEPHRA-186 Prefix shell variables and functions
    Signed-off-by: Chris Gianelloni <chris@cask.co>

--- bin/tephra-env.sh
+++ bin/tephra-env.sh
@@ -27,31 +27,31 @@
 export IDENT_STRING=$USER
 
 # Where log files are stored.  /var/log by default.
-export LOG_DIR=/tmp/tephra-$IDENT_STRING
+export TEPHRA_LOG_DIR=/tmp/tephra-$IDENT_STRING
 
 # The directory where pid files are stored. /var/run by default.
-export PID_DIR=/tmp
+export TEPHRA_PID_DIR=/tmp
 
 # Add any extra classes to the classpath
-# export EXTRA_CLASSPATH
+# export TEPHRA_EXTRA_CLASSPATH
 
 # Set the JVM heap size
-# export JAVA_HEAPMAX=-Xmx2048m
+# export TEPHRA_JAVA_HEAPMAX=-Xmx2048m
 
 # Additional runtime options
 #
 # GC logging options.
 # Uncomment the following two lines, making any desired changes, to enable GC logging output
-# export GC_LOG_OPTS="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:server-gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=50M"
-# export OPTS="$OPTS $GC_LOG_OPTS"
+# export TEPHRA_GC_LOG_OPTS="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:server-gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=50M"
+# export TEPHRA_OPTS="$TEPHRA_OPTS $TEPHRA_GC_LOG_OPTS"
 #
 # JMX options.
 # Uncomment the following two lines, making any desired changes, to enable remote JMX connectivity
-# export JMX_OPTS="-Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=13001"
-# export OPTS="$OPTS $JMX_OPTS"
+# export TEPHRA_JMX_OPTS="-Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=13001"
+# export TEPHRA_OPTS="$TEPHRA_OPTS $TEPHRA_JMX_OPTS"
 
 # Extra Java runtime options.
 # Below are what we set by default.  May only work with SUN JVM.
 # For more on why as well as other possible settings,
 # see http://wiki.apache.org/hadoop/PerformanceTuning
-export OPTS="$OPTS -XX:+UseConcMarkSweepGC"
+export TEPHRA_OPTS="$TEPHRA_OPTS -XX:+UseConcMarkSweepGC"
