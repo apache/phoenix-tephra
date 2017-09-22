e0572c496cbdb2f0296519338b94ce07dbd27f5b
Ali Anwar <anwar1@berkeley.edu>
2017-09-21 17:55:01 -0700

    Revert "TEPHRA-263 Enforce TTL, regardless of any in-progress transactions. Also Handle the case where TTL is longer than the duration from beginning of epoch to now."
    This reverts commit a346efe7560cc3b96349263669c4e15951ad401b.

--- tephra-core/src/test/java/org/apache/tephra/util/TxUtilsTest.java
+++ tephra-core/src/test/java/org/apache/tephra/util/TxUtilsTest.java
@@ -19,13 +19,8 @@
 package org.apache.tephra.util;
 
 import org.apache.tephra.Transaction;
-import org.apache.tephra.TxConstants;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -58,35 +53,5 @@ public class TxUtilsTest {
 
     tx = new Transaction(100, 110, new long[] {}, new long[] {}, Transaction.NO_TX_IN_PROGRESS);
     Assert.assertEquals(99, TxUtils.getPruneUpperBound(tx));
-  }
-
-  @Test
-  public void testTTL() {
-    long txIdsPerSecond = 1000 * TxConstants.MAX_TX_PER_MS;
-    byte[] family = new byte[] { 'd' };
-    long currentTxTimeSeconds = 100;
-    Transaction tx = new Transaction(100 * txIdsPerSecond, currentTxTimeSeconds * txIdsPerSecond,
-                                     new long[] {10 * txIdsPerSecond, 30 * txIdsPerSecond},
-                                     new long[] {80 * txIdsPerSecond, 90 * txIdsPerSecond},
-                                     80 * txIdsPerSecond);
-    int ttlSeconds = 60;
-    Map<byte[], Long> ttlByFamily = Collections.singletonMap(family, ttlSeconds * 1000L);
-    // ttl should only be impacted by the current transaction's id, and not by any older, in-progress transactions
-    Assert.assertEquals((currentTxTimeSeconds - ttlSeconds) * txIdsPerSecond,
-                        TxUtils.getOldestVisibleTimestamp(ttlByFamily, tx));
-  }
-
-  @Test
-  public void testLargeTTL() {
-    long txIdsPerSecond = 1000 * TxConstants.MAX_TX_PER_MS;
-    byte[] family = new byte[] { 'd' };
-    long currentTxTimeSeconds = 100;
-    Transaction tx = new Transaction(100 * txIdsPerSecond, currentTxTimeSeconds * txIdsPerSecond,
-                                     new long[] { }, new long[] { }, 100);
-    // ~100 years, so that the computed start timestamp is prior to 0 (epoch)
-    long ttlSeconds = TimeUnit.DAYS.toSeconds(365 * 100);
-    Map<byte[], Long> ttlByFamily = Collections.singletonMap(family, ttlSeconds * 1000L);
-    // oldest visible timestamp should be 0, not negative, because HBase timestamps can not be negative
-    Assert.assertEquals(0, TxUtils.getOldestVisibleTimestamp(ttlByFamily, tx));
   }
 }
