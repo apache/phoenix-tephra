df16b5be2146c4ae92d23d2c62a2cf3b32a0ced5
poorna <poorna@apache.org>
2018-04-25 18:58:52 -0700

    TEPHRA-266 Identify log messages when multiple instances of Tephra run on a single HBase cluster

--- tephra-core/src/main/java/org/apache/tephra/coprocessor/TransactionStateCache.java
+++ tephra-core/src/main/java/org/apache/tephra/coprocessor/TransactionStateCache.java
@@ -55,6 +55,7 @@ public class TransactionStateCache extends AbstractIdleService implements Config
   // snapshot refresh frequency in milliseconds
   private long snapshotRefreshFrequency;
   private boolean initialized;
+  private String logPrefix = "";
 
   public TransactionStateCache() {
   }
@@ -107,10 +108,10 @@ public class TransactionStateCache extends AbstractIdleService implements Config
                                                      TxConstants.Manager.DEFAULT_TX_SNAPSHOT_INTERVAL) * 1000;
         this.initialized = true;
       } else {
-        LOG.info("Could not load configuration");
+        LOG.info(prefixLog("Could not load configuration"));
       }
     } catch (Exception e) {
-      LOG.info("Failed to initialize TransactionStateCache due to: ", e);
+      LOG.info(prefixLog("Failed to initialize TransactionStateCache due to: "), e);
     }
   }
 
@@ -135,7 +136,7 @@ public class TransactionStateCache extends AbstractIdleService implements Config
             try {
               refreshState();
             } catch (IOException ioe) {
-              LOG.info("Error refreshing transaction state cache.", ioe);
+              LOG.info(prefixLog("Error refreshing transaction state cache."), ioe);
             }
           }
           try {
@@ -146,7 +147,7 @@ public class TransactionStateCache extends AbstractIdleService implements Config
             break;
           }
         }
-        LOG.info("Exiting thread " + getName());
+        LOG.info(prefixLog("Exiting thread " + getName()));
       }
     };
     this.refreshService.setDaemon(true);
@@ -164,18 +165,18 @@ public class TransactionStateCache extends AbstractIdleService implements Config
       TransactionVisibilityState currentState = storage.getLatestTransactionVisibilityState();
       if (currentState != null) {
         if (currentState.getTimestamp() < (now - 2 * snapshotRefreshFrequency)) {
-          LOG.info("Current snapshot is old, will force a refresh on next run.");
+          LOG.info(prefixLog("Current snapshot is old, will force a refresh on next run."));
           reset();
         } else {
           latestState = currentState;
-          LOG.info("Transaction state reloaded with snapshot from " + latestState.getTimestamp());
+          LOG.info(prefixLog("Transaction state reloaded with snapshot from " + latestState.getTimestamp()));
           if (LOG.isDebugEnabled()) {
-            LOG.debug("Latest transaction snapshot: " + latestState.toString());
+            LOG.debug(prefixLog("Latest transaction snapshot: " + latestState.toString()));
           }
           lastRefresh = now;
         }
       } else {
-        LOG.info("No transaction state found.");
+        LOG.info(prefixLog("No transaction state found."));
       }
     }
   }
@@ -183,5 +184,15 @@ public class TransactionStateCache extends AbstractIdleService implements Config
   @Nullable
   public TransactionVisibilityState getLatestState() {
     return latestState;
+  }
+
+  protected void setId(@Nullable String id) {
+    if (id != null) {
+      this.logPrefix = "[" + id + "] ";
+    }
+  }
+
+  private String prefixLog(String message) {
+    return logPrefix + message;
   }
 }
